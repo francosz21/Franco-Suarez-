c8d7cdc5273e0a78c11f94b96a050bd73ca891f2
Franco Suarez <francosz.2006@gmail.com>
2026-03-20 11:49:51 -0300

    Puntaje

--- questions.py
+++ questions.py
@@ -11,6 +11,7 @@ words = [
 ]
 word = random.choice(words)
 guessed = []
+puntaje=0
 attempts = 6
 print("¡Bienvenido al Ahorcado!")
 print()
@@ -22,10 +23,13 @@ while attempts > 0:
 			progress += letter + " "
 		else:
 			progress += "_ "
+			
 	print(progress)
 # Verificar si el jugador ya adivinó la palabra completa
 	if "_" not in progress:
 		print("¡Ganaste!")
+		puntaje=puntaje + 6 
+		print("puntaje=",puntaje)
 		break
 	print(f"Intentos restantes: {attempts}")
 	print(f"Letras usadas: {', '.join(guessed)}")
@@ -35,13 +39,18 @@ while attempts > 0:
 	elif letter in word:
 		guessed.append(letter)
 		print("¡Bien! Esa letra está en la palabra.")
+		print("puntaje=",puntaje)
 	elif len(letter)>1 or not letter.isalpha(): #condicion por si se ingresa algo que no sea 1 sola letra
 		print ("No puedes poner numeros o caracteres que no sean letras (solo 1 letra a la vez)")
-		
+		print("puntaje=",puntaje)
 	else:
 		guessed.append(letter)
 		attempts -= 1
+		puntaje=puntaje-1
 		print("Esa letra no está en la palabra.")
+		print("puntaje=",puntaje)
 	print()
 else:
 	print(f"¡Perdiste! La palabra era: {word}")
+	puntaje=0 
+	print ('tu puntaje fue de: ',puntaje)
